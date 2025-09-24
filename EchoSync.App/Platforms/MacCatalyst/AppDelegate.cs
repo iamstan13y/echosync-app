8c09ffa8d3f559b171375cf0f12099a16d24bcbf
iamstan13y <iamstan13y@gmail.com>
2025-09-24 15:18:13 -0700

    Add project files.

---
+++ EchoSync.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EchoSync.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
